728b9a24bfea43143f8564fba98c14f8a66c7378
Enrico Torres <enricoftorres0101@gmail.com>
2026-02-26 16:29:59 -0300

    feat(ros.js): add createTopic function and publish functions for cmdVel, TTS, neckControl, and faceEmotion

--- src/ros.js
+++ src/ros.js
@@ -19,10 +19,46 @@ export const TOPICS = Object.freeze({
   },
   neckControl: {
     name: "/updateNeck",
-    type: "std_msgs/Float64Multiarray",
+    type: "std_msgs/Float64MultiArray",
   },
   faceEmotion: {
     name: "/fbot_face/emotion",
     type: "std_msgs/String",
   },
 });
+
+export function createTopic(name, messageType, options = {}) {
+  return new ROSLIB.Topic({ ros, name, messageType, ...options });
+}
+
+const _cmdVel = createTopic(
+  TOPICS.cmdVel.name,
+  TOPICS.cmdVel.type
+);
+export function publishCmdVel(linear, angular) {
+  _cmdVel.publish(new ROSLIB.Message({ linear, angular }));
+}
+
+const _ttsSpeak = createTopic(
+  TOPICS.ttsSpeak.name,
+  TOPICS.ttsSpeak.type
+);
+export function publishTTS(text) {
+  _ttsSpeak.publish(new ROSLIB.Message({ data: text }));
+}
+
+const _updateNeck = createTopic(
+  TOPICS.neckControl.name,
+  TOPICS.neckControl.type,
+);
+export function publishUpdateNeck(position) {
+  _updateNeck.publish(new ROSLIB.Message({ data: position }));
+}
+
+const _faceEmotion = createTopic(
+  TOPICS.faceEmotion.name,
+  TOPICS.faceEmotion.type,
+);
+export function publishFaceEmotion(emotion) {
+  _faceEmotion.publish(new ROSLIB.Message({ data: emotion }));
+}
---
+++ src/updateNeck.js
@@ -0,0 +1,3 @@
+import { publishUpdateNeck } from "./ros.js";
+
+publishUpdateNeck([150, 150]);
