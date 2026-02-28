ae0d05bea050c8f9964d8d566fafeb8fa197d45c
Enrico Torres <enricoftorres0101@gmail.com>
2026-02-28 01:53:17 -0300

    feat(emotionController.js): add emotion controller to manage and publish emotions for the application

---
+++ src/emotionController.js
@@ -0,0 +1,26 @@
+import { publishFaceEmotion } from "./ros.js";
+
+export const EMOTIONS = Object.freeze([
+  "happy",
+  "sad",
+  "neutral",
+  "surprised",
+  "angry",
+  "suspicious",
+  "sleepy",
+]);
+
+let _currentEmotion = "neutral";
+
+export function getCurrentEmotion() {
+  return _currentEmotion;
+}
+
+export function setEmotion(emotion) {
+  if (!EMOTIONS.includes(emotion)) {
+    throw new Error(`Invalid emotion: ${emotion}. Use: ${EMOTIONS.join(", ")}`);
+  }
+
+  _currentEmotion = emotion;
+  publishFaceEmotion(emotion);
+}
