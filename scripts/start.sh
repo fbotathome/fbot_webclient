6c4d98a409dd67e782a45503143d64469e8dc295
Enrico Torres <enricoftorres0101@gmail.com>
2026-03-11 16:22:15 +0000

    feat(start.sh): add web video server launch to the start script for enhanced functionality

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,7 +12,9 @@ echo "Acesse: http://$LOCAL_IP:$PORT?robot_ip=$LOCAL_IP"
 
 ros2 launch rosbridge_server rosbridge_websocket_launch.xml & BRIDGE_PID=$!
 
+ros2 run web_video_server web_video_server --ros-args -p port:=8081 -p default_stream_type:=mjpeg & VIDEO_PID=$!
+
 python3 -m http.server "$PORT" --bind "$HOST" --directory "$PROJECT_DIR"
 
 
-kill $BRIDGE_PID
+kill $BRIDGE_PID $VIDEO_PID
