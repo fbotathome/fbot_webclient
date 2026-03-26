b401f0e6feaec6b89a03b67bee8a6d1f5ca480b8
Enrico Torres <enricoftorres0101@gmail.com>
2026-03-26 17:29:02 -0300

    feat(config): add video server URL and hostname fallback

--- src/config.js
+++ src/config.js
@@ -1,12 +1,9 @@
 const params = new URLSearchParams(window.location.search);
-const robotIp = params.get("robot_ip");
-
-if (!robotIp) {
-  throw new Error("The 'robot_ip' parameter is required in the URL.")
-}
+const robotIp = params.get("robot_ip") || window.location.hostname;
 
 const CONFIG = Object.freeze({
   rosbridgeUrl: `ws://${robotIp}:9090`,
+  videoServerUrl: `http://${robotIp}:8081`,
   reconnectTimeout: 3000,
 });
 
